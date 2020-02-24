50b62253be56af3d07a847d19f455fb5defc715c
KRASAVA <vladimirova.a-a@yandex.ru>
2020-02-24 16:33:19 +0300

    Enable filter through new buildings, cities and price on Admin page

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class AuthorAdmin(admin.ModelAdmin):
     readonly_fields = ['construction_year']
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
     list_editable = ['new_building']
+    list_filter = ('new_building', 'town', 'price')
 
 
 
